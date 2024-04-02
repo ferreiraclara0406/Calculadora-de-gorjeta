1b2936941e6415a3ea404648ac7b73f2ac3a866e
ferreira_clara <clarinhafs2018@gmail.com>
2024-04-01 23:32:18 -0300

    quarto commit js

--- script.js
+++ script.js
@@ -33,4 +33,17 @@ botoesGorjeta.forEach(botao => {
 })
 
 function receberPorcentagem(evento) {
-    
+    console.log(evento.target.value)
+}
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
+    console.log(porcentagem)
+} 
